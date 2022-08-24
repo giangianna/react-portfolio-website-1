13bc05f418815f9dbd365d9b5425571985ebd3ef
Gian Gianna <gian.ule@gmail.com>
2022-08-24 15:51:10 +0700

    Create all components and fixing root

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import ReactDOM from "react"
+// import { render } from 'react-dom';
+// import App from './App'
+// import './index.css'
+
+// render(<App/>, document.querySelector("#root"));
+
+// React v18
+import { createRoot } from 'react-dom/client';
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App/>, document.querySelector("#root"));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
