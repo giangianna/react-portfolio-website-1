4edfcada114cc7225cc100e9af46177253adac4f
Gian Gianna <gian.ule@gmail.com>
2022-08-24 19:33:30 +0700

    Header Component + Installing React Icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me1.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <h1>Gian Gianna Pallan Pallas</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA/>
+        <HeaderSocials/>
 
         <div className="me">
           <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {BsLinkedin, BsFacebook} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://id.linkedin.com/in/abangbule" target="_blank"></a>
-        <a href="https://github.com/giangianna" target="_blank"></a>
-        <a href="https://dribbble.com" target="_blank"></a>
+        <a href="https://id.linkedin.com/in/abangbule" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/giangianna" target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.facebook.com/junot.schmitt/" target="_blank" rel="noreferrer"><BsFacebook/></a>
     </div>
   )
 }
