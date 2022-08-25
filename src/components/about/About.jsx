9b299c43831a317ed28a700bdd97e6fcf2020f1e
Gian Gianna <gian.ule@gmail.com>
2022-08-25 11:33:49 +0700

    Fixing CSS Cards

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -13,9 +13,9 @@ const About = () => {
 
       <div className='container about__container'>
         <div className="about__me">
-        <div className="about__me-image">
-          <img src={ME} alt="About Me Image" />
-        </div>
+          <div className="about__me-image">
+            <img src={ME} alt="About Me Image" />
+          </div>
         </div>
         <div className="about__content">
           <div className='about__cards'>
