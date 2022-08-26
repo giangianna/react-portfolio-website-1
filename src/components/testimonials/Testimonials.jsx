17134663bf613bf0e07ac12b5b92761aa5d52b6b
Gian Gianna <gian.ule@gmail.com>
2022-08-26 09:12:48 +0700

    Testimonials Component JSX

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="AVATAR One" />
+            <h5>Ernets Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
+              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
+              similique, iure exercitationem voluptatibus molestias sit.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR2} alt="AVATAR Two" />
+            <h5>Ernets Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
+              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
+              similique, iure exercitationem voluptatibus molestias sit.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR3} alt="AVATAR Three" />
+            <h5>Ernets Achiever</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
+              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
+              similique, iure exercitationem voluptatibus molestias sit.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
