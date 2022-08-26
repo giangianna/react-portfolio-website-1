ca0ff232b75eb3f4d031057279e401fde612a19e
Gian Gianna <gian.ule@gmail.com>
2022-08-26 09:23:28 +0700

    Testimonials Component CSS

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import "./testimonials.css";
 import AVTR1 from "../../assets/avatar1.jpg";
 import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
-import AVTR4 from "../../assets/avatar4.jpg";
+// import AVTR4 from "../../assets/avatar4.jpg";
 
 const Testimonials = () => {
   return (
@@ -15,38 +15,38 @@ const Testimonials = () => {
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR1} alt="AVATAR One" />
-            <h5>Ernets Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
-              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
-              similique, iure exercitationem voluptatibus molestias sit.
-            </small>
           </div>
+          <h5>Ernets Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            accusamus consectetur nesciunt illum magni enim fugiat explicabo
+            deleniti eius fugit beatae, reiciendis pariatur nisi similique, iure
+            exercitationem voluptatibus molestias sit.
+          </small>
         </article>
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR2} alt="AVATAR Two" />
-            <h5>Ernets Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
-              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
-              similique, iure exercitationem voluptatibus molestias sit.
-            </small>
           </div>
+          <h5>Ernets Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            accusamus consectetur nesciunt illum magni enim fugiat explicabo
+            deleniti eius fugit beatae, reiciendis pariatur nisi similique, iure
+            exercitationem voluptatibus molestias sit.
+          </small>
         </article>
         <article className="testimonial">
           <div className="client__avatar">
             <img src={AVTR3} alt="AVATAR Three" />
-            <h5>Ernets Achiever</h5>
-            <small className="client__review">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatem accusamus consectetur nesciunt illum magni enim fugiat
-              explicabo deleniti eius fugit beatae, reiciendis pariatur nisi
-              similique, iure exercitationem voluptatibus molestias sit.
-            </small>
           </div>
+          <h5>Ernets Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            accusamus consectetur nesciunt illum magni enim fugiat explicabo
+            deleniti eius fugit beatae, reiciendis pariatur nisi similique, iure
+            exercitationem voluptatibus molestias sit.
+          </small>
         </article>
       </div>
     </section>
