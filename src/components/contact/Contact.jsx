70c920092920c63d319f2d5cb652bf29754de627
Gian Gianna <gian.ule@gmail.com>
2022-08-26 13:21:36 +0700

    Contact Component CSS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,48 +13,62 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>gian.ule@gmail.com</h5>
-            <a href="mailto:gian.ule@gmail.com" target="_blank">
+            <a
+              href="mailto:gian.ule@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
               Send a message
             </a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className="contact__option-icon" />
             <h4>Messenger</h4>
             <h5>Gian Gianna</h5>
-            <a href="https://m.me/junot.schmitt" target="_blank">
+            <a
+              href="https://m.me/junot.schmitt"
+              target="_blank"
+              rel="noreferrer"
+            >
               Send a message
             </a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className="contact__option-icon" />
             <h4>WhatsApp</h4>
             <h5>+62812 9173 7972</h5>
             <a
               href="https://api.whatsapp.com/send?phone=6281291737972"
               target="_blank"
+              rel="noreferrer"
             >
               Send a message
             </a>
           </article>
         </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
       {/* END OF CONTACT OPTION */}
-      <form action="">
-        <input type="text" name="name" placeholder="Your Full Name" required />
-        <input type="email" name="email" placeholder="Your Email" required />
-        <textarea
-          name="message"
-          rows="7"
-          placeholder="Your Message"
-          required
-        ></textarea>
-        <button type="submit" className="btn btn-primary">
-          Send Message
-        </button>
-      </form>
     </section>
   );
 };
